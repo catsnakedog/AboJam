76ae7a58a709888004b4106ee19d361512a17b3d
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2024-12-18 15:06:54 +0900

    타일 데이터 수정
    이제 Tile Class로 관리

--- Assets/Scripts/Define/StaticData.cs
+++ Assets/Scripts/Define/StaticData.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static EnumData;
 
 public static class StaticData
 {
-    public static int[,] MapData;
+    public static Tile[,] MapData;
     public static int Garu;
+}
+
+public class Tile
+{
+    public TileType Type;
 }
