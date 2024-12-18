2a19041396e7a04e0953a3ca70766c983a9880fb
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2024-12-18 14:24:24 +0900

    feat : 공용 데이터

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticData
+{
+    public static int[,] MapData;
+    public static int Garu;
+}
