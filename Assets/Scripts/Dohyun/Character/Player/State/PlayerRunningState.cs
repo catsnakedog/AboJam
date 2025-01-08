87dd5493a055f0e3229816de5c7efa7fb8c5edb5
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2025-01-08 22:52:08 +0900

    Player 리펙토링중
    아직 제작 ㄴㄴ

--- Assets/Scripts/Dohyun/Character/Player/State/PlayerRunningState.cs
+++ Assets/Scripts/Dohyun/Character/Player/State/PlayerRunningState.cs
@@ -4,20 +4,13 @@ public class PlayerRunningState : IPlayerState
 {
     public void Enter(Player player)
     {
-        Debug.Log("Player entered Running state.");
-        player.NotifyObservers("Running");
     }
 
     public void Update(Player player)
     {
-        if (player.MovementInput == Vector2.zero)
-        {
-            player.ChangeState(new IdleState());
-        }
     }
 
     public void Exit(Player player)
     {
-        Debug.Log("Player exited Running state.");
     }
 }
