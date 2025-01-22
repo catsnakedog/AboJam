f4d4e1565b862d5db5faed911687c15b2173de29
Park No Min <onlymutalisk@gmail.com>
2025-01-22 14:13:19 +0900

    Feat: Add Import (불러오기) & Export (덮어쓰기)
    1. 불러오기 : 서버 DB >> 런타임 DB
    
    # 다음 생성되는 인스턴스부터 런타임 DB >> 인스턴스
    
    2. 덮어쓰기 : 런타임 DB >> 인스턴스
    
    # 런타임 DB >> 현재 존재하는 모든 인스턴스 (즉시 적용 & 초기화)

--- Assets/Scripts/Nomin/Module/Database/Database_AboJam.cs
+++ Assets/Scripts/Nomin/Module/Database/Database_AboJam.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.InputSystem.HID;
+using UnityEngine.UIElements;
 using UnityEngine.Windows;
 
 /// <summary>
@@ -54,114 +55,128 @@ public class Database_AboJam : MonoBehaviour
         {
             DataSet dataSet = dbms.GetDataSet();
 
-            ImportHP();
-            ImportAbocado();
-            ImportGunner();
-            ImportAuto();
-            ImportGuard();
-            ImportSplash();
-            ImportHeal();
-
-            void ImportHP()
-            {
-                DataTable dataTable = dataSet.Tables["HP"];
-                dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["HPID"] };
-                foreach (Table_HP hp in HP)
-                {
-                    DataRow dataRow = dataTable.Rows.Find(hp.HPID);
-                    hp.Hp_max = Convert.ToSingle(dataRow["Hp_max"]);
-                    hp.HideFullHP = Convert.ToBoolean(dataRow["HideFullHP"]);
-                }
-            }
-            void ImportAbocado()
-            {
-                DataTable dataTable = dataSet.Tables["Abocado"];
-                dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["abocadoID"] };
-                foreach (Table_Abocado abocado in Abocado)
-                {
-                    DataRow dataRow = dataTable.Rows.Find(abocado.abocadoID);
-                    abocado.level = Enum.Parse<EnumData.Abocado>(Convert.ToString(dataRow["level"]));
-                    abocado.quality = Convert.ToInt32(dataRow["quality"]);
-                    abocado.quality_max = Convert.ToInt32(dataRow["quality_max"]);
-                    abocado.harvest = Convert.ToInt32(dataRow["harvest"]);
-                    abocado.harvestPlus = Convert.ToInt32(dataRow["harvestPlus"]);
-                }
-            }
-            void ImportGunner()
-            {
-                DataTable dataTable = dataSet.Tables["Gunner"];
-                dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["gunnerID"] };
-                foreach (Table_Gunner gunner in Gunner)
-                {
-                    DataRow dataRow = dataTable.Rows.Find(gunner.gunnerID);
-                    gunner.delay = Convert.ToSingle(dataRow["delay"]);
-                    gunner.delay_fire = Convert.ToSingle(dataRow["delay_fire"]);
-                    gunner.detection = Convert.ToSingle(dataRow["detection"]);
-                    gunner.subCount = Convert.ToInt32(dataRow["subCount"]);
-                }
-            }
-            void ImportAuto()
-            {
-                DataTable dataTable = dataSet.Tables["Auto"];
-                dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["autoID"] };
-                foreach (Table_Auto auto in Auto)
-                {
-                    DataRow dataRow = dataTable.Rows.Find(auto.autoID);
-                    auto.delay = Convert.ToSingle(dataRow["delay"]);
-                    auto.detection = Convert.ToSingle(dataRow["detection"]);
-                    auto.angle = Convert.ToSingle(dataRow["angle"]);
-                    auto.subCount = Convert.ToInt32(dataRow["subCount"]);
-                    auto.subCountPlus = Convert.ToInt32(dataRow["subCountPlus"]);
-                }
-            }
-            void ImportGuard()
-            {
-                DataTable dataTable = dataSet.Tables["Guard"];
-                dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["guardID"] };
-                foreach (Table_Guard guard in Guard)
-                {
-                    DataRow dataRow = dataTable.Rows.Find(guard.guardID);
-                    guard.hpMultiply = Convert.ToSingle(dataRow["hpMultiply"]);
-                }
-            }
-            void ImportSplash()
-            {
-                DataTable dataTable = dataSet.Tables["Splash"];
-                dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["splashID"] };
-                foreach (Table_Splash splash in Splash)
-                {
-                    DataRow dataRow = dataTable.Rows.Find(splash.splashID);
-                    splash.delay = Convert.ToSingle(dataRow["delay"]);
-                    splash.detection = Convert.ToSingle(dataRow["detection"]);
-                }
-            }
-            void ImportHeal()
-            {
-                DataTable dataTable = dataSet.Tables["Heal"];
-                dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["healID"] };
-                foreach (Table_Heal heal in Heal)
-                {
-                    DataRow dataRow = dataTable.Rows.Find(heal.healID);
-                    heal.delay = Convert.ToSingle(dataRow["delay"]);
-                    heal.detection = Convert.ToSingle(dataRow["detection"]);
-                    heal.ratio = Convert.ToSingle(dataRow["ratio"]);
-                }
-            }
+            ImportHP(dataSet);
+            ImportAbocado(dataSet);
+            ImportGunner(dataSet);
+            ImportAuto(dataSet);
+            ImportGuard(dataSet);
+            ImportSplash(dataSet);
+            ImportHeal(dataSet);
         }
         catch (Exception) { Debug.Log("서버와의 연결이 원활하지 않거나, 잘못된 데이터가 존재합니다."); throw; }
     }
+
     /// <summary>
-    /// 런타임 DB / HP 테이블 / HPID 레코드 => HP 인스턴스
+    /// 런타임 데이터베이스의 모든 데이터를 인스턴스에 덮어씁니다.
     /// </summary>
+    public void Export()
+    {
+        foreach (HP item in global::HP.instances) if (item.isActiveAndEnabled) item.Load();
+        foreach (Abocado item in global::Abocado.instances) if (item.isActiveAndEnabled) item.Load();
+        foreach (Gunner item in global::Gunner.instances) if (item.isActiveAndEnabled) item.Load();
+        foreach (Auto item in global::Auto.instances) if (item.isActiveAndEnabled) item.Load();
+        foreach (Guard item in global::Guard.instances) if (item.isActiveAndEnabled) item.Load();
+        foreach (Splash item in global::Splash.instances) if (item.isActiveAndEnabled) item.Load();
+        foreach (Heal item in global::Heal.instances) if (item.isActiveAndEnabled) item.Load();
+    }
+
+    // 서버 DB / 각 테이블 / ID 레코드 => 런타임 DB
+    // 기획자가 인게임에서 Import 누를 시 호출
+    public void ImportHP(DataSet dataSet)
+    {
+        DataTable dataTable = dataSet.Tables["HP"];
+        dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["HPID"] };
+        foreach (Table_HP hp in HP)
+        {
+            DataRow dataRow = dataTable.Rows.Find(hp.HPID);
+            hp.Hp_max = Convert.ToSingle(dataRow["Hp_max"]);
+            hp.HideFullHP = Convert.ToBoolean(dataRow["HideFullHP"]);
+        }
+    }
+    public void ImportAbocado(DataSet dataSet)
+    {
+        DataTable dataTable = dataSet.Tables["Abocado"];
+        dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["abocadoID"] };
+        foreach (Table_Abocado abocado in Abocado)
+        {
+            DataRow dataRow = dataTable.Rows.Find(abocado.abocadoID);
+            abocado.level = Enum.Parse<EnumData.Abocado>(Convert.ToString(dataRow["level"]));
+            abocado.quality = Convert.ToInt32(dataRow["quality"]);
+            abocado.quality_max = Convert.ToInt32(dataRow["quality_max"]);
+            abocado.harvest = Convert.ToInt32(dataRow["harvest"]);
+            abocado.harvestPlus = Convert.ToInt32(dataRow["harvestPlus"]);
+        }
+    }
+    public void ImportGunner(DataSet dataSet)
+    {
+        DataTable dataTable = dataSet.Tables["Gunner"];
+        dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["gunnerID"] };
+        foreach (Table_Gunner gunner in Gunner)
+        {
+            DataRow dataRow = dataTable.Rows.Find(gunner.gunnerID);
+            gunner.delay = Convert.ToSingle(dataRow["delay"]);
+            gunner.delay_fire = Convert.ToSingle(dataRow["delay_fire"]);
+            gunner.detection = Convert.ToSingle(dataRow["detection"]);
+            gunner.subCount = Convert.ToInt32(dataRow["subCount"]);
+        }
+    }
+    public void ImportAuto(DataSet dataSet)
+    {
+        DataTable dataTable = dataSet.Tables["Auto"];
+        dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["autoID"] };
+        foreach (Table_Auto auto in Auto)
+        {
+            DataRow dataRow = dataTable.Rows.Find(auto.autoID);
+            auto.delay = Convert.ToSingle(dataRow["delay"]);
+            auto.detection = Convert.ToSingle(dataRow["detection"]);
+            auto.angle = Convert.ToSingle(dataRow["angle"]);
+            auto.subCount = Convert.ToInt32(dataRow["subCount"]);
+            auto.subCountPlus = Convert.ToInt32(dataRow["subCountPlus"]);
+        }
+    }
+    public void ImportGuard(DataSet dataSet)
+    {
+        DataTable dataTable = dataSet.Tables["Guard"];
+        dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["guardID"] };
+        foreach (Table_Guard guard in Guard)
+        {
+            DataRow dataRow = dataTable.Rows.Find(guard.guardID);
+            guard.hpMultiply = Convert.ToSingle(dataRow["hpMultiply"]);
+        }
+    }
+    public void ImportSplash(DataSet dataSet)
+    {
+        DataTable dataTable = dataSet.Tables["Splash"];
+        dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["splashID"] };
+        foreach (Table_Splash splash in Splash)
+        {
+            DataRow dataRow = dataTable.Rows.Find(splash.splashID);
+            splash.delay = Convert.ToSingle(dataRow["delay"]);
+            splash.detection = Convert.ToSingle(dataRow["detection"]);
+        }
+    }
+    public void ImportHeal(DataSet dataSet)
+    {
+        DataTable dataTable = dataSet.Tables["Heal"];
+        dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["healID"] };
+        foreach (Table_Heal heal in Heal)
+        {
+            DataRow dataRow = dataTable.Rows.Find(heal.healID);
+            heal.delay = Convert.ToSingle(dataRow["delay"]);
+            heal.detection = Convert.ToSingle(dataRow["detection"]);
+            heal.ratio = Convert.ToSingle(dataRow["ratio"]);
+        }
+    }
+
+    // 런타임 DB / 각 테이블 / ID 레코드 => 인스턴스
+    // 기획자가 인게임에서 Export 누를 시 호출
+    // 인스턴스가 Load (풀에서 꺼내지거나, 처음 생성) 될 때 마다 호출
     public void ExportHP(string HPID, ref float hp_max, ref bool hideFullHp)
     {
         Table_HP data = HP.FirstOrDefault(hp => hp.HPID == HPID);
         hp_max = data.Hp_max;
         hideFullHp = data.HideFullHP;
     }
-    /// <summary>
-    /// 런타임 DB / Abocado 테이블 / AbocadoID 레코드 => Abocado 인스턴스
-    /// </summary>
     public void ExportAbocado(string abocadoID, ref EnumData.Abocado level, ref int quality, ref int quality_max, ref int harvest, ref int harvestPlus)
     {
         Table_Abocado data = Abocado.FirstOrDefault(abocado => abocado.abocadoID == abocadoID);
@@ -171,9 +186,6 @@ public class Database_AboJam : MonoBehaviour
         harvest = data.harvest;
         harvestPlus = data.harvestPlus;
     }
-    /// <summary>
-    /// 런타임 DB / Gunner 테이블 / GunnerID 레코드 => Gunner 인스턴스
-    /// </summary>
     public void ExportGunner(string gunnerID, ref float delay, ref float delay_fire, ref float detection, ref int subCount)
     {
         Table_Gunner data = Gunner.FirstOrDefault(gunner => gunner.gunnerID == gunnerID);
@@ -182,9 +194,6 @@ public class Database_AboJam : MonoBehaviour
         detection = data.detection;
         subCount = data.subCount;
     }
-    /// <summary>
-    /// 런타임 DB / Auto 테이블 / AutoID 레코드 => Auto 인스턴스
-    /// </summary>
     public void ExportAuto(string autoID, ref float delay, ref float detection, ref float angle, ref int subCount, ref int subCountPlus)
     {
         Table_Auto data = Auto.FirstOrDefault(auto => auto.autoID == autoID);
@@ -194,26 +203,17 @@ public class Database_AboJam : MonoBehaviour
         subCount = data.subCount;
         subCountPlus = data.subCountPlus;
     }
-    /// <summary>
-    /// 런타임 DB / Guard 테이블 / GuardID 레코드 => Guard 인스턴스
-    /// </summary>
     public void ExportGuard(string guardID, ref float hpMultiply)
     {
         Table_Guard data = Guard.FirstOrDefault(guard => guard.guardID == guardID);
         hpMultiply = data.hpMultiply;
     }
-    /// <summary>
-    /// 런타임 DB / Splash 테이블 / SplashID 레코드 => Splash 인스턴스
-    /// </summary>
     public void ExportSplash(string splashID, ref float delay, ref float detection)
     {
         Table_Splash data = Splash.FirstOrDefault(splash => splash.splashID == splashID);
         delay = data.delay;
         detection = data.detection;
     }
-    /// <summary>
-    /// 런타임 DB / Heal 테이블 / HealID 레코드 => Heal 인스턴스
-    /// </summary>
     public void ExportHeal(string healID, ref float delay, ref float detection, ref float ratio)
     {
         Table_Heal data = Heal.FirstOrDefault(heal => heal.healID == healID);
