54349b5d0d0caadafe4c00940ab7828680d44b21
Park No Min <onlymutalisk@gmail.com>
2025-02-20 17:48:44 +0900

    Feat: Add Windowed Mode
    1. 창모드로 전환이 가능하며, 자유자재로 크기를 조절할 수 있습니다. 비율은 자동으로 조정됩니다.

--- Assets/Scripts/Nomin/Module/Sizer.cs
+++ Assets/Scripts/Nomin/Module/Sizer.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 /// <summary>
@@ -7,14 +9,38 @@ using UnityEngine;
 /// </summary>
 public class Sizer : MonoBehaviour
 {
+    /* Dependency */
+#if UNITY_STANDALONE_WIN
+    // 플랫폼이 윈도우일 경우, 애플리케이션 창 크기를 조절 중 인지 검사합니다.
+    [DllImport("user32.dll")]
+    private static extern short GetAsyncKeyState(int vKey);
+    private const int VK_LBUTTON = 0x01; // 마우스 왼쪽 버튼
+    private bool IsResizing()
+    {
+        if (GetAsyncKeyState(VK_LBUTTON) != 0) return true;
+        else return false;
+    }
+#endif
+
+    /* Field & Property */
     [SerializeField] GameObject[] objects; // 자동으로 화면에 맞게 조절 될 오브젝트 추가
     [SerializeField] float resolutionWidth; // 에디터 기준 해상도
     [SerializeField] float resolutionHeight; // 에디터 기준 해상도
+    int lastWidth = 0;
+    int lastHeight = 0;
 
     /* Initializer & Finalizer & Updater */
-    private void Start()
+    private void Update()
     {
-        foreach (GameObject obj in objects) Size(obj);
+        if (Screen.width != lastWidth || Screen.height != lastHeight)
+        {
+#if UNITY_STANDALONE_WIN
+            if (IsResizing()) return; // 윈도우 창 크기 조절이 끝나고 수행
+#endif
+
+            SetScreenRatio(16, 9);
+            foreach (GameObject obj in objects) Size(obj);
+        }
     }
 
     /* Public Method */
@@ -25,6 +51,26 @@ public class Sizer : MonoBehaviour
     {
         ScaleWithScreenSize(go);
         PositionWithScreenSize(go);
+    }
+    /// <summary>
+    /// 화면 비율을 width : height 로 변경합니다.
+    /// </summary>
+    public void SetScreenRatio(float width, float height)
+    {
+        // 가로 변동 시 비율에 맞춰 세로 조절
+        if (lastWidth != Screen.width)
+        {
+            lastWidth = Screen.width;
+            lastHeight = Mathf.RoundToInt((height / width) * Screen.width);
+            Screen.SetResolution(lastWidth, lastHeight, false);
+        }
+        // 세로 변동 시 비율에 맞춰 가로 조절
+        else if (lastHeight != Screen.height)
+        {
+            lastHeight = Screen.height;
+            lastWidth = Mathf.RoundToInt((width / height) * Screen.height);
+            Screen.SetResolution(lastWidth, lastHeight, false);
+        }
     }
 
     /* Private Method */
@@ -45,5 +91,4 @@ public class Sizer : MonoBehaviour
 
         go.transform.localPosition = new Vector3(newPosX, newPosY, go.transform.localPosition.z);
     }
-
 }
