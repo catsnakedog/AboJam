49bfb877ddaf83a31a9928ad46587cf17a8a5966
Park No Min <onlymutalisk@gmail.com>
2025-08-08 11:43:38 +0900

    Docs: Change Game Start Key to Double Tab

--- Assets/Scripts/Nomin/Main/Background.cs
+++ Assets/Scripts/Nomin/Main/Background.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class Background : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    /* Event */
+
+    private event Action eventTap;
+    private event Action eventDoubleTap;
+
+    /* Field */
+
+    [SerializeField] private float doubleTapThreshold;
+    private DateTime lastTouchTime;
+
+    private void Start()
     {
-        SceneManager.LoadScene(sceneName);
+        Init();
+    }
+
+    private void Init()
+    {
+        lastTouchTime = DateTime.Now;
+        eventDoubleTap = LoadGame;
+    }
+
+    /* Public Method */
+
+    public void Tap()
+    {
+        DateTime currentTouchTime = DateTime.Now;
+        bool isDoubleTap = CheckIsDoubleTap(currentTouchTime);
+        lastTouchTime = currentTouchTime;
+
+        if (isDoubleTap) eventDoubleTap?.Invoke();
+        else eventTap?.Invoke();
+    }
+
+    /* Private Method */
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene("Nomin");
         StaticData.Init();
+    }
+
+    private bool CheckIsDoubleTap(DateTime currentTouchTime)
+    {
+        TimeSpan timedif = currentTouchTime - lastTouchTime;
+        return timedif.TotalSeconds < doubleTapThreshold;
     }
 }
